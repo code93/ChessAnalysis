9d8a8c380738023b503931d2833674e4aae514ba
code93 <shubhambirmi7@gmail.com>
2020-05-01 00:36:33 +0530

    Residual Plots, All Possible Combinations, number of steps dependency

--- plotRes.R
+++ plotRes.R
@@ -1,92 +1,85 @@
-
-png("plot1.png")
-par(mfrow=c(2,2))
-
-plot(fit1$residuals, revfit1$residuals, 
-     xlab = "Black dependent on White", 
-     ylab = "White dependent on Black",
-     col=c("red", "blue"),
-     main="Residual Plot")
-
-plot(fit2$residuals, revfit2$residuals, 
-     xlab = "Black dependent on White", 
-     ylab = "White dependent on Black",
-     col=c("red", "blue"),
-     main="Residual Win Plot")
-
-plot(fit3$residuals, revfit3$residuals, 
-     xlab = "Black dependent on White", 
-     ylab = "White dependent on Black",
-     col=c("red", "blue"),
-     main="Residual lose Plot")
-
-plot(fit4$residuals, revfit4$residuals, 
-     xlab = "Black dependent on White", 
-     ylab = "White dependent on Black",
-     col=c("red", "blue"),
-     main="Residual draw Plot")
-
-dev.off()
-
-
-png("plot2.png")
-par(mfrow=c(2,2))
-
-plot(fit1King$residuals, revfit1King$residuals, 
-     xlab = "Black dependent on White", 
-     ylab = "White dependent on Black",
-     col=c("red", "blue"),
-     main="Residual Plot")
-
-plot(fit2King$residuals, revfit2King$residuals, 
-     xlab = "Black dependent on White", 
-     ylab = "White dependent on Black",
-     col=c("red", "blue"),
-     main="Residual Win Plot")
-
-plot(fit3King$residuals, revfit3King$residuals, 
-     xlab = "Black dependent on White", 
-     ylab = "White dependent on Black",
-     col=c("red", "blue"),
-     main="Residual lose Plot")
-
-plot(fit4King$residuals, revfit4King$residuals, 
-     xlab = "Black dependent on White", 
-     ylab = "White dependent on Black",
-     col=c("red", "blue"),
-     main="Residual draw Plot")
-
-dev.off()
+plotResidual <- function(x,name){
+        
+        png(paste0((name),".png"))
+        par(mfrow=c(2,2))
+        
+        plot(get(paste0("fit1", x))$residuals, get(paste0("revfit1", x))$residuals, 
+             xlab = "Black dependent on White", 
+             ylab = "White dependent on Black",
+             col=c("red", "blue"),
+             main="Residual Plot")
+        
+        plot(get(paste0("fit2", x))$residuals, get(paste0("revfit2", x))$residuals, 
+             xlab = "Black dependent on White", 
+             ylab = "White dependent on Black",
+             col=c("red", "blue"),
+             main="Residual Win Plot")
+        
+        plot(get(paste0("fit3", x))$residuals, get(paste0("revfit3", x))$residuals, 
+             xlab = "Black dependent on White", 
+             ylab = "White dependent on Black",
+             col=c("red", "blue"),
+             main="Residual lose Plot")
+        
+        plot(get(paste0("fit4", x))$residuals, get(paste0("revfit4", x))$residuals, 
+             xlab = "Black dependent on White", 
+             ylab = "White dependent on Black",
+             col=c("red", "blue"),
+             main="Residual draw Plot")
+        
+        dev.off()
+}
 
 
-png("plot3.png")
-par(mfrow=c(2,2))
+plotResidual(x= "", name="All_VS_All")
+plotResidual(x= "Bishop", name="Bishop_VS_All")
+plotResidual(x= "King", name="King_VS_All")
+plotResidual(x= "Knight", name="Knight_VS_All")
+plotResidual(x= "Castle", name="Castle_VS_All")
+plotResidual(x= "Queen", name="Queen_VS_All")
+plotResidual(x= "Rook", name="Rook_VS_All")
 
-plot(fit1KK$residuals, revfit1KK$residuals, 
-     xlab = "Black dependent on White", 
-     ylab = "White dependent on Black",
-     col=c("red", "blue"),
-     main="Residual Plot")
+plotResidual(x= "BB", name="Bishop_VS_Bishop")
+plotResidual(x= "BK", name="Bishop_VS_King")
+plotResidual(x= "BN", name="Bishop_VS_Knight")
+plotResidual(x= "BO", name="Bishop_VS_Castle")
+plotResidual(x= "BQ", name="Bishop_VS_Queen")
+plotResidual(x= "BR", name="Bishop_VS_Rook")
 
-plot(fit2KK$residuals, revfit2KK$residuals, 
-     xlab = "Black dependent on White", 
-     ylab = "White dependent on Black",
-     col=c("red", "blue"),
-     main="Residual Win Plot")
+plotResidual(x= "KB", name="King_VS_Bishop")
+plotResidual(x= "KK", name="King_VS_King")
+plotResidual(x= "KN", name="King_VS_Knight")
+plotResidual(x= "KO", name="King_VS_Castle")
+plotResidual(x= "KQ", name="King_VS_Queen")
+plotResidual(x= "KR", name="King_VS_Rook")
 
-plot(fit3KK$residuals, revfit3KK$residuals, 
-     xlab = "Black dependent on White", 
-     ylab = "White dependent on Black",
-     col=c("red", "blue"),
-     main="Residual lose Plot")
+plotResidual(x= "NB", name="Knight_VS_Bishop")
+plotResidual(x= "NK", name="Knight_VS_King")
+plotResidual(x= "NN", name="Knight_VS_Knight")
+plotResidual(x= "NO", name="Knight_VS_Castle")
+plotResidual(x= "NQ", name="Knight_VS_Queen")
+plotResidual(x= "NR", name="Knight_VS_Rook")
 
-plot(fit4KK$residuals, revfit4KK$residuals, 
-     xlab = "Black dependent on White", 
-     ylab = "White dependent on Black",
-     col=c("red", "blue"),
-     main="Residual draw Plot")
+plotResidual(x= "OB", name="Castle_VS_Bishop")
+plotResidual(x= "OK", name="Castle_VS_King")
+plotResidual(x= "ON", name="Castle_VS_Knight")
+plotResidual(x= "OO", name="Castle_VS_Castle")
+plotResidual(x= "OQ", name="Castle_VS_Queen")
+plotResidual(x= "OR", name="Castle_VS_Rook")
 
-dev.off()
+plotResidual(x= "QB", name="Queen_VS_Bishop")
+plotResidual(x= "QK", name="Queen_VS_King")
+plotResidual(x= "QN", name="Queen_VS_Knight")
+plotResidual(x= "QO", name="Queen_VS_Castle")
+plotResidual(x= "QQ", name="Queen_VS_Queen")
+plotResidual(x= "QR", name="Queen_VS_Rook")
+
+plotResidual(x= "RB", name="Rook_VS_Bishop")
+plotResidual(x= "RK", name="Rook_VS_King")
+plotResidual(x= "RN", name="Rook_VS_Knight")
+plotResidual(x= "RO", name="Rook_VS_Castle")
+plotResidual(x= "RQ", name="Rook_VS_Queen")
+plotResidual(x= "RR", name="Rook_VS_Rook")
 
 
 
